e45b583d96306d7010be713fd9418fb9200bd00e
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-09-07 22:44:05 -0300

    Cambios para deploy

--- src/componentes/Home/FiltersCategorys/FilterCategory.jsx
+++ src/componentes/Home/FiltersCategorys/FilterCategory.jsx
@@ -12,9 +12,8 @@ import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import error from "../../../404.png";
 import { useState } from "react";
-import CircularProgress from '@mui/material/CircularProgress';
-import Footer from '../../Footer'
-
+import CircularProgress from "@mui/material/CircularProgress";
+import Footer from "../../Footer";
 
 export default function FilterCategory() {
   const services = useSelector((state) => state.services);
@@ -50,104 +49,112 @@ export default function FilterCategory() {
   useEffect(() => {
     dispatch(getAllServices());
   }, [dispatch]);
-  
+
   if (services.length === 0)
     return (
-      <div style={{backgroundColor: 'rgba(0, 0, 0, 0.644)'}}>
+      <div style={{ backgroundColor: "rgba(0, 0, 0, 0.644)" }}>
         <Navbar />
-      <div style={{display:'flex' ,height: '80vh', alignItems:'center', justifyContent: 'center'}}>
-        <CircularProgress />
+        <div
+          style={{
+            display: "flex",
+            height: "80vh",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </div>
+        <Footer />
       </div>
+    );
+  else
+    return (
+      <div className="service-container-full">
+        <Navbar />
+
+        <FormCategory />
+        <div className="container-services">
+          <div style={{ margin: "15px", color: "#fff" }}>
+            <Typography variant="h5">{param.name}</Typography>
+          </div>
+          {filterUsers.length === 0 ? (
+            <Box
+              className="empty-container"
+              sx={{
+                textAlign: "center",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                maxWidth: "80%",
+                position: "relative",
+                margin: "40px auto",
+              }}
+            >
+              <Box>
+                <Typography variant="h5" mb={5}>
+                  No se encuentra ningun servicio para esta categoria{" "}
+                  <NavLink className="linkk" to="/home/createService">
+                    se el primero en postularte!
+                  </NavLink>
+                </Typography>
+                {<img src={error} alt="?" width="182px" height="182px" />}
+              </Box>
+            </Box>
+          ) : (
+            userSlice?.map((e) => {
+              return (
+                <div className="cards-services" key={e.id}>
+                  <img
+                    src={e.user?.img}
+                    alt="No tiene"
+                    width="64px"
+                    height="64px"
+                  />
+                  <h3>Servicio: {e.name}</h3>
+                  <h4>{e.user?.firstName}</h4>
+                  <p>{e.description}</p>
+                  <p>${e.price}</p>
+                  <Link to={`/home/services/${e.id}`}>
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "#354152", margin: "5px" }}
+                    >
+                      Haz tu reserva
+                    </Button>
+                  </Link>
+                  <Link to={`/home/public/${e.user?.id}`}>
+                    <Button
+                      variant="contained"
+                      sx={{ backgroundColor: "#354152" }}
+                    >
+                      Ver Perfil
+                    </Button>
+                  </Link>
+                </div>
+              );
+            })
+          )}
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#354152", margin: "5px", color: "#fff" }}
+            onClick={handleAnterior}
+          >
+            {"<"}
+          </Button>
+          <span style={{ color: "#fff" }}>
+            {pages} de {paginas}
+          </span>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#354152", margin: "5px", color: "#fff" }}
+            onClick={handleSiguiente}
+          >
+            {">"}
+          </Button>
+        </div>
         <Footer/>
       </div>
     );
-  else  
-  return (
-    <div className="service-container-full">
-      <Navbar />
-
-        <FormCategory />
-      <div className="container-services">
-        <div style={{ margin: "15px", color: "#fff" }}>
-          <Typography variant="h5">{param.name}</Typography>
-        </div>
-        {filterUsers.length === 0 ? (
-          <Box
-            className="empty-container"
-            sx={{
-              textAlign: "center",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              maxWidth: "80%",
-              position: "relative",
-              margin: "40px auto",
-            }}
-          >
-            <Box>
-              <Typography variant="h5" mb={5}>
-                No se encuentra ningun servicio para esta categoria{" "}
-                <NavLink className="linkk" to="/home/createService">
-                  se el primero en postularte!
-                </NavLink>
-              </Typography>
-              {<img src={error} alt="?" width="182px" height="182px" />}
-            </Box>
-          </Box>
-        ) : (
-          userSlice?.map((e) => {
-            return (
-              <div className="cards-services" key={e.id}>
-                <img
-                  src={e.user?.img }
-                  alt="No tiene"
-                  width="64px"
-                  height="64px"
-                />
-                <h3>Servicio: {e.name}</h3>
-                <h4>{e.user?.firstName}</h4>
-                <p>{e.description}</p>
-                <p>${e.price}</p>
-                <Link to={`/home/services/${e.id}`}>
-                  <Button
-                    variant="contained"
-                    sx={{ backgroundColor: "#354152", margin: "5px" }}
-                  >
-                    Haz tu reserva
-                  </Button>
-                </Link>
-                <Link to={`/home/public/${e.user?.id}`}>
-                  <Button
-                    variant="contained"
-                    sx={{ backgroundColor: "#354152" }}
-                  >
-                    Ver Perfil
-                  </Button>
-                </Link>
-              </div>
-            );
-          })
-        )}
-      </div>
-      <div style={{ textAlign: "center" }}>
-        <Button
-          variant="contained"
-          sx={{ backgroundColor: "#354152", margin: "5px", color: "#fff" }}
-          onClick={handleAnterior}
-        >
-          {"<"}
-        </Button>
-        <span style={{ color: "#fff" }}>
-          {pages} de {paginas}
-        </span>
-        <Button
-          variant="contained"
-          sx={{ backgroundColor: "#354152", margin: "5px", color: "#fff" }}
-          onClick={handleSiguiente}
-        >
-          {">"}
-        </Button>
-      </div>
-    </div>
-  );
 }
--- src/componentes/Home/Guardar.jsx
+++ src/componentes/Home/Guardar.jsx
@@ -6,27 +6,33 @@ import Footer from "../Footer";
 import Navbar from "../PrivateRoute/Navbar";
 import Reviews from "../reviews/Reviews";
 import Category from "./AuxHome/Category";
-import CircularProgress from '@mui/material/CircularProgress';
-import '../css/revwievs.css'
+import CircularProgress from "@mui/material/CircularProgress";
+import "../css/revwievs.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCategories } from "../../redux/actions";
-
 
 export default function Guardar() {
   const { user } = useAuth();
   const category = useSelector((state) => state.categories);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getAllCategories())
-  },[dispatch]);
+    dispatch(getAllCategories());
+  }, [dispatch]);
 
   if (category.length === 0)
     return (
-      <div style={{backgroundColor: 'rgba(0, 0, 0, 0.644)'}}>
+      <div style={{ backgroundColor: "rgba(0, 0, 0, 0.644)" }}>
         <Navbar />
-      <div style={{display:'flex' ,height: '80vh', alignItems:'center', justifyContent: 'center'}}>
-        <CircularProgress />
-      </div>
+        <div
+          style={{
+            display: "flex",
+            height: "80vh",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </div>
         <Footer />
       </div>
     );
@@ -38,12 +44,21 @@ export default function Guardar() {
           <Category />
         </div>
         <Box className="rev-section">
-          <Typography variant="h4" sx={{position: 'relative ',margin: '70px auto', borderBottom:'solid 2px black', paddingBottom: '40px', width:'700px'}}>Algunos comentarios de nuestros Usuarios</Typography>
+          <Typography
+            variant="h4"
+            sx={{
+              position: "relative ",
+              margin: "70px auto",
+              borderBottom: "solid 2px black",
+              paddingBottom: "40px",
+              width: "700px",
+            }}
+          >
+            Algunos comentarios de nuestros Usuarios
+          </Typography>
           <Reviews />
         </Box>
         <Footer />
       </div>
     );
 }
-
-
